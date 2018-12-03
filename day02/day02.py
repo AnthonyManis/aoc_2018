169857f7ae1a200b7d0ebd0b3475ad1c82f08f69
Anthony G Manis <anthonygmanis@gmail.com>
2018-12-02 21:16:51 -0800

    Day 2 part 2 outline/progress.

--- day02/day02.py
+++ day02/day02.py
@@ -6,65 +6,70 @@
 INPUTFILE = 'input.txt'
 
 def load_input(infile):
-    lines = []
-    with open(infile, 'r') as fp:
-        for line in fp:
-            line = line.strip()
-            if line:
-                lines.append(line)
+	lines = []
+	with open(infile, 'r') as fp:
+		for line in fp:
+			line = line.strip()
+			if line:
+				lines.append(line)
 
-        return lines
+	return lines
 
 # PART 1
 def part1(box_ids):
-    # key: box id, value: dict of character occurrence counts
-    box_stats = {}
-    for box in box_ids:
-        # Store occurrences of each letter for this box id here
-        occurrences = {}
-        # list of characters in the box id, in order?
-        characters = list(box)
-        for c in characters:
-            if c in occurrences:
-                occurrences[c] += 1
-            else:
-                occurrences[c] = 1
+	# key: box id, value: dict of character occurrence counts
+	box_stats = {}
+	for box in box_ids:
+		# Store occurrences of each letter for this box id here
+		occurrences = {}
+		# list of characters in the box id, in order?
+		characters = list(box)
+		for c in characters:
+			if c in occurrences:
+				occurrences[c] += 1
+			else:
+				occurrences[c] = 1
 
-            box_stats[box] = occurrences
-        
-    # Now find matches
-    match_count = {}
-    for key in box_stats:
-        for i in range(1,len(key)):
-            if i in box_stats[key].values():
-                if i in match_count:
-                    match_count[i] += 1
-                else:
-                    match_count[i] = 1
-        
-    print(match_count)
-    checksum = match_count[2] * match_count[3]
-    print("PART 1: " + str(checksum))
+			box_stats[box] = occurrences
+				
+	# Now find matches
+	match_count = {}
+		for key in box_stats:
+			for i in range(1,len(key)):
+				if i in box_stats[key].values():
+					if i in match_count:
+						match_count[i] += 1
+					else:
+						match_count[i] = 1
+				
+	print(match_count)
+	checksum = match_count[2] * match_count[3]
+	print("PART 1: " + str(checksum))
 
 # PART 2
 # Process the character arrays in parallel, comparing chars.
 # If the chars are different, keep a count of how many differences are found.
 # If the chars are identical, shove em into an output string.
 def part2(box_ids):
-    output = ""
-    for box1 in box_ids:
-        for box2 in box_ids:
-            if box1 != box2:
-                print(box1)
-                print(box2)
+	output = ""
+	for box1 in box_ids:
+		for box2 in box_ids:
+			if box1 != box2:
+				# List the characters
+				box1_chars = list(box1)
+				box2_chars = list(box2)
+				if len(box1_chars) > len(box2_chars):
+								
 
-    print("PART 2: " + str(output))
+
+
+	print("PART 2: " + str(output))
 
 if __name__ == '__main__':
-    print("EXAMPLE1: ")
-    example1_input = [ 'abcdef', 'bababc', 'abbcde', 'abcccd', 'aabcdd', 'abcdee', 'ababab' ]
-    part1(example1_input)
-    print("END OF EXAMPLES")
-    input = load_input(INPUTFILE)
-    part1(input)
-    part2(input)
+	print("EXAMPLE1: ")
+	example1_input = [ 'abcdef', 'bababc', 'abbcde', 'abcccd', 'aabcdd', 'abcdee', 'ababab' ]
+	part1(example1_input)
+	print("END OF EXAMPLES")
+	input = load_input(INPUTFILE)
+	part1(input)
+	part2(input)
