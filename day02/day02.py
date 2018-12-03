c8ef7a69a60ae4e0ae0a35bdb3879acd075b380d
Anthony G Manis <anthonygmanis@gmail.com>
2018-12-02 21:48:42 -0800

    Day 2 Part 2 complete.

--- day02/day02.py
+++ day02/day02.py
@@ -34,13 +34,13 @@ def part1(box_ids):
 				
 	# Now find matches
 	match_count = {}
-		for key in box_stats:
-			for i in range(1,len(key)):
-				if i in box_stats[key].values():
-					if i in match_count:
-						match_count[i] += 1
-					else:
-						match_count[i] = 1
+	for key in box_stats:
+		for i in range(1,len(key)):
+			if i in box_stats[key].values():
+				if i in match_count:
+					match_count[i] += 1
+				else:
+					match_count[i] = 1
 				
 	print(match_count)
 	checksum = match_count[2] * match_count[3]
@@ -58,17 +58,35 @@ def part2(box_ids):
 				# List the characters
 				box1_chars = list(box1)
 				box2_chars = list(box2)
-				if len(box1_chars) > len(box2_chars):
-								
+				box1_len = len(box1_chars)
+				box2_len = len(box2_chars)
+				length = box1_len if box1_len > box2_len else box2_len
+				differences = 0
+				matching_chars = ""
+				# Comparing characters
+				for i in range(0,length):
+					if box1_chars[i] and box2_chars[i] and box1_chars[i] == box2_chars[i]:
+						matching_chars += box1_chars[i]
+					else:
+						differences += 1
 
+				if differences == 1:
+					output1 = matching_chars
+					output2 = box1
+					output3 = box2
 
-
-	print("PART 2: " + str(output))
+	print("COMPARE STRINGS: ")
+	print(output2)
+	print(output3)
+	print("PART 2: " + str(output1))
 
 if __name__ == '__main__':
-	print("EXAMPLE1: ")
+	print("EXAMPLE 1: ")
 	example1_input = [ 'abcdef', 'bababc', 'abbcde', 'abcccd', 'aabcdd', 'abcdee', 'ababab' ]
 	part1(example1_input)
+	print("EXAMPLE 2: ")
+	example2_input = [ 'abcde', 'fghij', 'klmno', 'pqrst', 'fguij', 'axcye', 'wvxyz' ]
+	part2(example2_input)
 	print("END OF EXAMPLES")
 	input = load_input(INPUTFILE)
 	part1(input)
