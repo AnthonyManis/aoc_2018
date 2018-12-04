8a83da248ff7d02c3b38e876a6d38688dcddc144
Anthony G Manis <anthonygmanis@gmail.com>
2018-12-04 09:39:27 -0800

    Overlap test.

--- day03/day03.py
+++ day03/day03.py
@@ -21,7 +21,7 @@ def load_input(infile):
 class Rectangle:
 	"""A rectangle in four numbers... maybe a few more."""
 
-	def __init__(self, id = None, x = None, y = None , w = None , h = None):
+	def __init__(self, id = None, x = 0, y = 0, w = 0 , h = 0):
 		self.id = id
 		self.x = int(x)
 		self.y = int(y)
@@ -56,16 +56,43 @@ class Rectangle:
 		return False
 
 	def intersectionAsRectangle(self, other):
-		# Is other.left contained in self?
-			# How much overlap is there?
-		# Is other.right contained in self?
-			# How much overlap is there?
+		x_overlap = 0
+		y_overlap = 0
+		left_edge = 0
+		right_edge = 0
+		top_edge = 0
+		bottom_edge = 0
+		intersection = Rectangle(self.id + other.id)
 
+		# Is other.left contained in self?
+		if self.left() <= other.left() <= self.right():
+			left_edge = other.left()
+			right_edge = min(self.right(), other.right())
+		# Is other.right contained in self?
+		elif self.left() <= other.right() <= self.right():
+			right_edge = other.right()
+			left_edge = max(self.left(), other.left())
+		x_overlap = right_edge - left_edge
 
 		# Is other.top contained in self?
-			# How much overlap is there?
+		if self.top() <= other.top() <= self.bottom():
+			top_edge = other.top()
+			bottom_edge = min(self.bottom(), other.bottom())
 		# Is other.bottom contained in self?
-			# How much overlap is there?
+		elif self.top() <= other.bottom() <= self.bottom():
+			bottom_edge = other.bottom()
+			top_edge = max(self.top(), other.top())
+		y_overlap = bottom_edge - top_edge
+
+		if x_overlap and y_overlap:
+			intersection.x = left_edge
+			intersection.y = top_edge
+			intersection.w = right_edge - left_edge
+			intersection.h = bottom_edge - top_edge
+			return intersection
+		else:
+			return False
+		
 
 
 def part1(arg):
@@ -83,7 +110,14 @@ def part1(arg):
 
 	# Find overlap between two rectangles, and mark that area as a 1 in grid.
 	grid = [[0 for x in range(1000)] for y in range(1000)]
-	
+
+	r1 = Rectangle("#1", 1, 3, 4, 4)
+	r2 = Rectangle("#2", 3, 1, 4, 4)
+	r3 = Rectangle("#3", 5, 5, 2, 2)
+
+	print(r1.intersectionAsRectangle(r2))
+	print(r1.intersectionAsRectangle(r3))
+	print(r2.intersectionAsRectangle(r3))
 
 	print("PART 1: " + str(output))
 
