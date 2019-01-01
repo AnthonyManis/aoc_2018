ffeccdcfddbc902325cc2b3e2c651274010f3d7a
Anthony G Manis <anthonygmanis@gmail.com>
2018-12-31 19:57:18 -0800

    Day 5 part 1 completed, but I'm not happy.

--- day05/day05.py
+++ day05/day05.py
@@ -3,6 +3,8 @@
 # Advent of Code 2018 - Day 5
 #
 
+import re
+
 INPUTFILE = 'input.txt'
 
 def load_input(infile):
@@ -11,7 +13,6 @@ def load_input(infile):
 		while True:
 			char = fp.read(1)
 			if not char:
-				print('EOF')
 				break
 			if not char.isspace():
 				characters.append(char)
@@ -19,7 +20,26 @@ def load_input(infile):
 
 # PART 1
 def part1(characters):
-	print(characters)
+	print('Units before=', len(characters))
+	i = 0
+	while i < len(characters):
+		if i + 1 < len(characters):
+			c = characters[i]
+			n = characters[i+1]
+			# First we must know if the letter is the same, so ignore case.
+			if re.match(c, n, flags=re.IGNORECASE):
+				# Then again without ignorecase to check if case/'polarity' is different.
+				if not re.match(c, n):
+					del characters[i]
+					del characters[i]
+					# If we deleted some characters, maybe we should take a few steps back to analyze more characters.
+					# 1 step back plus 1 since we're about to increment at end of loop.
+					# This is a bit sloppy. It gives the right answer, but if you change it to for instance i -= 3 it gives the wrong answer.
+					i -= 2
+		i += 1
+	print('PART 1=', len(characters))
+
+	
 		
 if __name__ == '__main__':
 	input = load_input(INPUTFILE)
