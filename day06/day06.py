af6415d8c49ccc1dc1b916fdbc41e10c50056b46
Anthony G Manis <anthonygmanis@gmail.com>
2019-01-01 20:59:04 -0800

    Day 6 part 2 setup.

--- day06/day06.py
+++ day06/day06.py
@@ -72,9 +72,11 @@ def printGrid(grid):
 				line_string += grid[x][y]
 			print(line_string)
 
-# PART 1
-def part1(lines):
-	coords = {}
+coords = {}
+def labelCoords(lines):
+	# Don't do it twice.
+	if coords:
+		return
 	# Assign a single-character label to each starting node.
 	label_ascii = ord('A')
 	for line in lines:
@@ -86,6 +88,7 @@ def part1(lines):
 		# Next label
 		label_ascii += 1
 
+def findGridSize():
 	# Get lowest and highest x and y coordinates so we can just make a grid.
 	low_x = high_x = coords['A'][0]
 	low_y = high_y = coords['A'][1]
@@ -95,9 +98,23 @@ def part1(lines):
 		high_x = x if x > high_x else high_x
 		high_y = y if y > high_y else high_y
 
-	# Interestingly test input is of size (313,313)
-	# We'll have some extra space since the low_x and low_y are greater than 0,
-	# but it's acceptable to waste some space rather than deal with offsets and oob indices.
+	return (low_x, low_y), (high_x, high_y)
+
+def calculateDistanceBetween(x1, y1, x2, y2):
+	distance = 0
+	distance += abs(x2 - x1)
+	distance += abs(y2 - y1)
+	return distance
+
+# PART 1
+def part1(lines):
+
+	labelCoords(lines)
+
+	(_, _), (high_x, high_y) = findGridSize()
+
+	# It's acceptable to waste some space rather than deal with offsets and oob indices.
+	# So we'll start the 2D list at (0, 0)
 	grid_nodes_only = [ ['0' for y in range(high_y + 1)] for x in range(high_x + 1) ]
 
 	# Add initial nodes to the grid.
@@ -166,12 +183,16 @@ def part1(lines):
 		if label in valid_coords:
 			del valid_coords[label]
 
-	for x in range(len(grid)):
-		for y in range(len(grid[0])):
-			label = grid[x][y]
-			if label not in valid_coords:
-				grid[x][y] = ' '
+	# Alternative way to visualize grid: Don't show infinite spaces or dots.
+	#for x in range(len(grid)):
+		#for y in range(len(grid[0])):
+			#label = grid[x][y]
+			#if label not in valid_coords:
+				#grid[x][y] = ' '
 
+	printGrid(grid)
+
+	# Compute and print areas.
 	areas = {}
 	for label in valid_coords:
 		areas[label] = 0
@@ -180,8 +201,26 @@ def part1(lines):
 				if grid[x][y] == label:
 					areas[label] += 1
 		print(label, areas[label])
-	#printGrid(grid)
+
+def part2(lines):
+
+	labelCoords(lines)
+
+	(_, _), (high_x, high_y) = findGridSize()
+
+	# Make a grid with the running total score of each coordinate.
+	grid_scores = [ [0 for y in range(high_y)] for x in range(high_x) ]
+
+	# For each position in the grid, calculate it's distance to each point, and sum them for the score.
+	for x in range(len(grid_scores)):
+		for y in range(len(grid_scores[0])):
+			score = 0
+			for label in coords:
+				score += calculateDistanceBetween(x, y, coords[label][0], coords[label][1])
+
+	return
 
 if __name__ == '__main__':
 	input = load_input(INPUTFILE)
-	part1(input)
+	#part1(input)
+	part2(input)
